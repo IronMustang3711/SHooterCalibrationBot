75450eaa67715988f3161f3444e58b83acc00251
jasonnn <jasonmarkham@gmail.com>
2017-02-02 21:27:39 -0800

    post meetup code cleanup.

--- ShooterCalibrationBot/src/Robot.cpp
+++ ShooterCalibrationBot/src/Robot.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <memory>
 #include <string>
+#include <cmath>
 
 #include <Joystick.h>
 #include <SampleRobot.h>
@@ -11,10 +12,11 @@
 #include <CANTalon.h>
 #include <Talon.h>
 #include <PowerDistributionPanel.h>
+#include <Utility.h>
 
 /**
  While throttling the Talon in the positive direction, make sure the sensor speed is also positive.
-Additionally note the approximate sensor speed while the Talon is driven.
+ Additionally note the approximate sensor speed while the Talon is driven.
  getSpeed() will return the speed in RPM since the requirements in Section 17.2.1 are met.
 
  The SmartDashboard values can also be read in the roboRIO web-based configuration under Self-Tests.
@@ -24,123 +26,122 @@ Additionally note the approximate sensor speed while the Talon is driven.
  native units per rotation(resolution)= 4 * cycles per revolution,
  reqs. for unit scaling=ConfigEncoderCodesPerRev()
 
-rpm to native units:
+ rpm to native units:
 
-X_Rpm * 1/600 = revs per 100ms
-revs_per_100ms * native_units_per_rotation =  native units per 100ms
+ X_Rpm * 1/600 = revs per 100ms
+ revs_per_100ms * native_units_per_rotation =  native units per 100ms
 
-1023 = max out
+ 1023 = max out
 
-F-gain = (100% X 1023) / (native units per 100ms)
+ F-gain = (100% X 1023) / (native units per 100ms)
 
-then update F-gain
+ then update F-gain
 
-Remember “err” is in native units per 100ms. So an error of 900 units per 100ms equals an error of 131RPM since each rotation is 4096 units.
+ Remember “err” is in native units per 100ms. So an error of 900 units per 100ms equals an error of 131RPM since each rotation is 4096 units.
 
-Next we will add in P-gain so that the closed-loop can react to error.
+ Next we will add in P-gain so that the closed-loop can react to error.
  Suppose given our worst error so far (900 native units per 100ms), we want to respond with another 10% of throttle.
  Then our starting p-gain would be....
-(10% X 1023) / (900) = 0.113333
-Now let’s check our math, if the Talon SRX sees an error of 900 the P-term will be
-900 X 0.113333 = 102 (which is about 10% of 1023) P-gain = 0.113333
-Apply the P -gain programmatically using your preferred method.
-Now retest to see how well the closed-loop responds to varying loads.
-Double the P -gain until the system oscillates (too much) or until the system responds adequately.
-If the mechanism is moving to swiftly, you can add D-gain to smooth the motion. Start with 10x the p-gain.
-If the mechanism is not quite reaching the final target position (and P -gain cannot be increased further without hurting overall performance)
+ (10% X 1023) / (900) = 0.113333
+ Now let’s check our math, if the Talon SRX sees an error of 900 the P-term will be
+ 900 X 0.113333 = 102 (which is about 10% of 1023) P-gain = 0.113333
+ Apply the P -gain programmatically using your preferred method.
+ Now retest to see how well the closed-loop responds to varying loads.
+ Double the P -gain until the system oscillates (too much) or until the system responds adequately.
+ If the mechanism is moving to swiftly, you can add D-gain to smooth the motion. Start with 10x the p-gain.
+ If the mechanism is not quite reaching the final target position (and P -gain cannot be increased further without hurting overall performance)
  begin adding I-gain. Start with 1/100th of the P-gain.
 
  */
+
 class Robot: public frc::SampleRobot {
 	frc::Joystick stick { 0 };
-	CANTalon shooterController { 1 ,/*update rate in ms*/5};
-	frc::Talon loaderController { 5 };
-	frc::PowerDistributionPanel pdp{};
-
+	CANTalon shooterController { 1,/*update rate in ms*/5 };
+	//target ∈ [-4500,0];
+	double target = 0;
+	double motorOutput = 0;
+	double speed = 0;
+	int error = 0;
+	double timestamp = 0;
 
 public:
 	Robot() {
-		//Note SmartDashboard is not initialized here, wait until RobotInit to make SmartDashboard calls
+	}
 
+	/**
+	 * [0,0.1) -> [0,3000)
+	 * [0.1,1.0]->[3000,4500]
+	 */
+
+	double remapSliderValue(double input) {
+		return input < 0.1 ? input * 30000.0 : input * 1666.7 + 2833.3;
+	}
+
+	void telemetryUpdate() {
+		motorOutput = shooterController.GetOutputVoltage()
+				/ shooterController.GetBusVoltage();
+		speed = shooterController.GetSpeed();
+		error = shooterController.GetClosedLoopError();
+		timestamp = frc::Timer::GetFPGATimestamp();
+	}
+
+	void smartDashboardUpdate() {
+		SmartDashboard::PutNumber("TALON: motor output", motorOutput);
+		SmartDashboard::PutNumber("TALON: speed", speed);
+		SmartDashboard::PutNumber("TALON: target", target);
+		SmartDashboard::PutNumber("TALON: closed loop error", error);
 	}
 
 	void RobotInit() {
-	      shooterController.SetFeedbackDevice(CANTalon::QuadEncoder);
-	      shooterController.ConfigEncoderCodesPerRev(20);
-	      shooterController.SetSensorDirection(false);
-			shooterController.SetPosition(0);
-			shooterController.SetControlMode(CANSpeedController::kSpeed);
+		shooterController.SetFeedbackDevice(CANTalon::QuadEncoder);
+		shooterController.ConfigEncoderCodesPerRev(20);
+		shooterController.SetSensorDirection(false);
+		shooterController.SetPosition(0);
+		shooterController.SetControlMode(CANSpeedController::kSpeed);
 
 //Nominal Closed-Loop Output: Promotes the minimal or weakest motor-output during closed-loop.
-	      shooterController.ConfigNominalOutputVoltage(+0., -2.0);
-	      shooterController.ConfigPeakOutputVoltage(-2.0, -12.0);
-			/* set the allowable closed-loop error,
-			 * Closed-Loop output will be neutral within this range.
-			 * See Table in Section 17.2.1 for native units per rotation.
-			 */
-	      shooterController.SetAllowableClosedLoopErr(0); /* always servo */
-	      shooterController.SetF(1.51);
-	      shooterController.SetP(0.98);
-	      shooterController.SetI(0.0);
-	      shooterController.SetD(0.1);
-	      shooterController.SetCloseLoopRampRate(0.0);
-
+		shooterController.ConfigNominalOutputVoltage(+0., -2.0);
+		shooterController.ConfigPeakOutputVoltage(-2.0, -15.0);
+		/* set the allowable closed-loop error,
+		 * Closed-Loop output will be neutral within this range.
+		 * See Table in Section 17.2.1 for native units per rotation.
+		 */
+		shooterController.SetAllowableClosedLoopErr(0); /* always servo */
+		shooterController.SetF(1.51);
+		shooterController.SetP(0.98);
+		shooterController.SetI(0.05);
+		shooterController.SetD(0.1);
+		shooterController.SetCloseLoopRampRate(0.0);
+		shooterController.SetIzone(60);
+		//TODO: reverse, get rid of negative numbers.
+		//shooterController.SetClosedLoopOutputDirection()
 
 	}
 	void shooterUpdate() {
-		float sliderValue = (-stick.GetRawAxis(3)+1)*0.5;   // [0,1]
+		//sliderValue ∈ [0,1]
+		double sliderValue = (-stick.GetRawAxis(3) + 1) * 0.5;
+		//target ∈ [-5227,0];
+		target = -5227.0 * log10(9.0 * sliderValue + 1.0);
 
+		shooterController.Set(target);
 
-
-
-//		SmartDashboard::PutNumber("slider", sliderValue);
-		double motorOutput = shooterController.GetOutputVoltage()
-				/ shooterController.GetBusVoltage();
-
-		SmartDashboard::PutNumber("TALON: motor output", motorOutput);
-		SmartDashboard::PutNumber("TALON: speed", shooterController.GetSpeed());
-
-
-			double target = -sliderValue * 4500.0; //max RPM
-
-			if(sliderValue > 0.1){
-				target = -sliderValue*1500 - 3000;
-			}
-
-			shooterController.Set(target);
-
-			SmartDashboard::PutNumber("TALON: target", target);
-
-//		} else {
-//			shooterController.SetControlMode(CANSpeedController::kPercentVbus);
-//			shooterController.Set(sliderValue);
-//		}
-		SmartDashboard::PutNumber("TALON: closed loop error",
-				shooterController.GetClosedLoopError());
-
-
-
-		SmartDashboard::PutNumber("encoder velocity",
-				shooterController.GetEncVel());
-
-		//SmartDashboard::PutNumber("current",pdp.GetCurrent(12));
 	}
 
-	/*
-	 * Runs the motors with arcade steering.
-	 */
 	void OperatorControl() override {
 
 		while (IsOperatorControl() && IsEnabled()) {
-			if (stick.GetRawButton(1)) {
-				loaderController.Set(1.0);
-			} else {
-				loaderController.Set(0.0);
-			}
+
 			shooterUpdate();
+			telemetryUpdate();
+			smartDashboardUpdate();
 
 			frc::Wait(0.005);
 		}
+	}
+
+	void Test() {
+		LiveWindow::GetInstance()->AddActuator("ROBOT", "SHOOTER",
+				shooterController);
 	}
 
 };
